cf0fdde2965cf4b2f09a545c93d90821fa956845
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-03-27 02:29:06 -0600

    Require dataclass-builder>=1.1.0.
    Need this version for the addition of MISSING constant comparison.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=[
         'cached_property',
         'dataclasses;python_version=="3.6"',
-        'dataclass-builder>=1.0.0',
+        'dataclass-builder>=1.1.0',
         'numpy',
         'scipy',
         'wrapt',
