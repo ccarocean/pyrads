d6985737e60bf5eccf0d4ec5546ad33a4825abfb
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-07-12 11:34:24 -0600

    Change rads.config.text_parsers to take tag attrs.

--- rads/config/text_parsers.py
+++ rads/config/text_parsers.py
@@ -1,7 +1,8 @@
 """Parsers for text within a tag.
 
-The parsers in this file should all take a string and return a value or take
-one or more parser functions and return a new parsing function.
+The parsers in this file should all take a string and a mapping of attributes
+for the tag and return a value or take one or more parser functions and return
+a new parsing function.
 
 If a parser deals with generic XML then it should be rads.config.xml_parsers
 and if it returns a rads.config.xml_parsers.Parser but is not generic then it
@@ -10,14 +11,16 @@ belongs in rads.config.grammar.
 
 from datetime import datetime
 from numbers import Real
-from typing import Any, Callable, List, Sequence, TypeVar
+from typing import (Any, Callable, Dict, Iterable, List, Mapping, Optional,
+                    Sequence, TypeVar)
 
 import numpy as np  # type: ignore
 
 from .tree import Compress, Cycles, Range, ReferencePass, Repeat, Unit
+from .._utility import fortran_float
 
-__all__ = ['list_of', 'range_of', 'types', 'compress', 'cycles', 'nop',
-           'ref_pass', 'repeat', 'time', 'unit']
+__all__ = ['lift', 'list_of', 'one_of', 'range_of', 'compress', 'cycles',
+           'nop', 'ref_pass', 'repeat', 'time', 'unit']
 
 T = TypeVar('T')
 
@@ -28,23 +31,88 @@ T = TypeVar('T')
 # text parsers together and return a new text parser.
 
 
-def list_of(parser: Callable[[str], T]) -> Callable[[str], List[T]]:
-    def _parser(string: str) -> List[T]:
-        return [parser(s) for s in string.split()]
+def lift(string_parser: Callable[[str], T]) \
+        -> Callable[[str, Mapping[str, str]], T]:
+    """Lift a simple string parser to a text parser that accepts attributes.
+
+    This is very similar to lifting a plain function into a monad.
+
+    Parameters
+    ----------
+    string_parser
+        A simple parser that takes a string and returns a value.
+
+    Returns
+    -------
+    function
+        The given :paramref:`string_parser` with an added argument to accept
+        and ignore the attributes for the text tag.
+
+    """
+    def _parser(string: str, _: Mapping[str, str]) -> T:
+        return string_parser(string)
+
+    _parser._lifted = string_parser.__qualname__  # type: ignore
+    return _parser
+
+
+def list_of(parser: Callable[[str, Mapping[str, str]], T],
+            sep: Optional[str] = None) \
+        -> Callable[[str, Mapping[str, str]], List[T]]:
+    """Convert parser into a parser of lists.
+
+    Parameters
+    ----------
+    parser
+        Original parser.
+    sep
+        Item delimiter.  Default is to separate by one or more spaces.
+
+    Returns
+    -------
+    function
+        The new parser of delimited lists.
+
+    """
+    def _parser(string: str, attr: Mapping[str, str]) -> List[T]:
+        return [parser(s, attr) for s in string.split(sep=sep)]
 
     return _parser
 
 
-def range_of(parser: Callable[[str], Real]) -> Callable[[str], Range]:
-    def _parser(string: str) -> Range:
-        minmax = [parser(s) for s in string.split()]
-        if len(minmax) == 0:
+def range_of(parser: Callable[[str, Mapping[str, str]], Real]) \
+        -> Callable[[str, Mapping[str, str]], Range]:
+    """Create a range parser from a given parser for each range element.
+
+    The resulting parser will parse space separated lists of length 2 and use
+    the given :paramref:`parser` for both elements.
+
+    Parameters
+    ----------
+    parser
+        Parser to use for the min and max values.
+
+    Returns
+    -------
+    function
+        New range parser.
+
+    Raises
+    ------
+    ValueError
+        Resulting parser raises this if given a string that does not contain
+        two space separated elements.
+
+    """
+    def _parser(string: str, attr: Mapping[str, str]) -> Range:
+        minmax = [parser(s, attr) for s in string.split()]
+        if not minmax:
             raise ValueError(
                 'ranges require exactly 2 values, but none were given')
-        elif len(minmax) == 1:
+        if len(minmax) == 1:
             raise ValueError(
                 'ranges require exactly 2 values, but only 1 was given')
-        elif len(minmax) > 2:
+        if len(minmax) > 2:
             raise ValueError(
                 'ranges require exactly 2 values, '
                 f'but {len(minmax)} were given')
@@ -53,19 +121,50 @@ def range_of(parser: Callable[[str], Real]) -> Callable[[str], Range]:
     return _parser
 
 
-def types(parsers: Sequence[Callable[[str], Any]]) \
-        -> Callable[[str], Any]:
-    parser_types = ', '.join(parser.__qualname__ for parser in parsers)
+def one_of(parsers: Iterable[Callable[[str, Mapping[str, str]], Any]]) \
+        -> Callable[[str, Mapping[str, str]], Any]:
+    """Convert parsers into a parser that tries each one in sequence.
 
-    def _parser(string: str) -> Any:
+    .. note::
+
+        Each parser will be tried in sequence.  The next parser will be tried
+        if TypeError, ValueError, or KeyError is raised.
+
+    Parameters
+    ----------
+    parsers
+        A sequence of parsers the new parsers should try in order.
+
+    Returns
+    -------
+    function
+        The new parser which tries each of the given :paramref:`parsers` in
+        order until once succeeds.
+
+    Raises
+    ------
+    TypeError
+        Resulting parser raises this if given a string that cannot be parsed by
+        any of the given :paramref:`parsers`.
+
+    """
+    def _parser(string: str, attr: Mapping[str, str]) -> Any:
         for parser in parsers:
             try:
-                return parser(string)
-            except (TypeError, ValueError):
+                return parser(string, attr)
+            except (TypeError, ValueError, KeyError):
                 pass
 
-        raise TypeError(f"cannot convert '{string}' to any of the following "
-                        f"types: {parser_types}")
+        # build list of parser types
+        parser_types = []
+        for parser in parsers:
+            try:
+                parser_types.append(parser._lifted)  # type: ignore
+            except AttributeError:
+                parser_types.append(parser.__qualname__)
+
+        raise ValueError(f"cannot convert '{string}' to any of the following "
+                         f"types: {', '.join(parser_types)}")
 
     return _parser
 
@@ -76,72 +175,287 @@ def types(parsers: Sequence[Callable[[str], Any]]) \
 # a value.
 
 
-def compress(string: str) -> Compress:
+def compress(string: str, _: Mapping[str, str]) -> Compress:
+    """Parse a string into a :class:`Compress` object.
+
+    Parameters
+    ----------
+    string
+        String to parse into a :class:`Compress` object.  It should be in the
+        following form:
+
+            <type:type> [scale_factor:float] [add_offset:float]
+
+        where only the first value is required and should be one of the
+        following 4 character RADS data type values:
+
+            * `int1` - maps to numpy.int8
+            * `int2` - maps to numpy.int16
+            * `int4` - maps to numpy.int32
+            * `real` - maps to numpy.float32
+            * `dble` - maps to numpy.float64
+
+        The attribute mapping of the tag the string came from.  Not currently
+        used by this function.
+    _
+        Mapping of tag attributes.  Not used by this function.
+
+    Returns
+    -------
+    Compress
+        A new :class:`Compress` object created from the parsed string.
+
+    Raises
+    ------
+    ValueError
+        If the <type> is not in the given :paramref:`string` or if too many
+        values are in the :paramref:`string`.  Also, if one of the values
+        cannot be converted.
+
+    """
     parts = string.split()
-    if len(parts) > 3:
-        raise TypeError(
-            "too many values given, expected only 'type', "
-            "'scale_factor', and 'add_offset'")
     try:
-        return Compress(
-            *(f(s) for f, s in zip((_rads_type, float, float), parts)))
+        funcs: Iterable[Callable[[str], Any]] = (
+            _rads_type, fortran_float, fortran_float, lambda x: x)
+        return Compress(*(f(s) for f, s in zip(funcs, parts)))
     except TypeError:
-        raise TypeError("'missing 'type'")
+        if len(parts) > 3:
+            raise ValueError(
+                "too many values given, expected only 'type', "
+                "'scale_factor', and 'add_offset'")
+        raise ValueError("'missing 'type'")
 
 
-def cycles(string: str) -> Cycles:
+def cycles(string: str, _: Mapping[str, str]) -> Cycles:
+    """Parse a string into a :class:`Cycles` object.
+
+    Parameters
+    ----------
+    string
+        String to parse into a :class:`Cycles` object.  It should be in the
+        following form:
+
+        .. code-block:: text
+
+            <first cycle in phase> <last cycle in phase>
+    _
+        Mapping of tag attributes.  Not used by this function.
+
+    Returns
+    -------
+    Cycles
+        A new :class:`Cycles` object created from the parsed string.
+
+    Raises
+    ------
+    ValueError
+        If the wrong number of values are given in the :paramref:`string` or
+        one of the values is not parsable to an integer.
+
+    """
     try:
         return Cycles(*(int(s) for s in string.split()))
     except TypeError:
         num_values = len(string.split())
         if num_values == 0:
-            raise TypeError("missing 'first' cycle")
+            raise ValueError("missing 'first' cycle")
         if num_values == 1:
-            raise TypeError("missing 'last' cycle")
-        raise TypeError(
+            raise ValueError("missing 'last' cycle")
+        raise ValueError(
             "too many cycles given, expected only 'first' and 'last'")
 
 
-def nop(string: str) -> str:
+def nop(string: str, _: Mapping[str, str]) -> str:
+    """No operation parser, returns given string unchanged.
+
+    This exists primarily as a default for when no parser is given as the use
+    of `lift(str)` is recommended when the parsed value is supposed to be a
+    string.
+
+    Parameters
+    ----------
+    string
+        String to return.
+    _
+        Mapping of tag attributes.  Not used by this function.
+
+    Returns
+    -------
+    str
+        The given :paramref:`string`.
+
+    """
     return string
 
 
-def ref_pass(string: str) -> ReferencePass:
+def ref_pass(string: str, _: Mapping[str, str]) -> ReferencePass:
+    """Parse a string into a :class:`ReferencePass` object.
+
+    Parameters
+    ----------
+    string
+        String to parse into a:class:`ReferencePass` object.  It should be in
+        the following form:
+
+        .. code-block:: text
+
+            <yyyy-mm-ddTHH:MM:SS> <lon> <cycle> <pass> [absolute orbit number]
+
+        where the last element is optional and defaults to 1.  The date can
+        also be missing seconds, minutes, and hours.
+    _
+        Mapping of tag attributes.  Not used by this function.
+
+    Returns
+    -------
+    ReferencePass
+        A new :class:`ReferencePass` object created from the parsed string.
+
+    Raises
+    ------
+        If the wrong number of values are given in the :paramref:`string` or
+        one of the values is not parsable.
+
+    """
     parts = string.split()
-    if len(parts) > 5:
-        raise TypeError("too many values given, expected only 'time', "
-                        "'longitude', 'cycle number', 'pass number', and "
-                        "optionally 'absolute orbit number'")
     try:
-        funcs: Sequence[Callable[[str], Any]] = (time, float, int, int, int)
+        funcs: Sequence[Callable[[str], Any]] = (
+            _time, float, int, int, int, lambda x: x)
         return ReferencePass(*(f(s) for f, s in zip(funcs, parts)))
     except TypeError:
         if not parts:
-            raise TypeError("missing 'time' of reference pass")
+            raise ValueError("missing 'time' of reference pass")
         if len(parts) == 1:
-            raise TypeError("missing 'longitude' of reference pass")
+            raise ValueError(
+                "missing equator crossing 'longitude' of reference pass")
         if len(parts) == 2:
-            raise TypeError("missing 'cycle number' of reference pass")
-        # len(parts) == 3
-        raise TypeError("missing 'pass number' of reference pass")
+            raise ValueError("missing 'cycle number' of reference pass")
+        if len(parts) == 3:
+            raise ValueError("missing 'pass number' of reference pass")
         # absolute orbit number is defaulted in ReferencePass
+        raise ValueError("too many values given, expected only 'time', "
+                         "'longitude', 'cycle number', 'pass number', and "
+                         "optionally 'absolute orbit number'")
 
 
-def repeat(string: str) -> Repeat:
+def repeat(string: str, _: Mapping[str, str]) -> Repeat:
+    """Parse a string into a :class:`Repeat` object.
+
+    Parameters
+    ----------
+    string
+        String to parse into a :class:`Repeat` object. It should be in the
+        following form:
+
+        .. code-block:: text
+
+            <days:float> <passes:int> [longitude drift per cycle:float]
+
+        where the last value is optional.
+    _
+        Mapping of tag attributes.  Not used by this function.
+
+    Returns
+    -------
+    Repeat
+        A new :class:`Repeat` object created from the parsed string.
+
+    Raises
+    ------
+    ValueError
+        If the wrong number of values are given in the :paramref:`string` or
+        one of the values is not parsable.
+
+    """
     parts = string.split()
-    if len(parts) > 3:
-        raise TypeError(
-            "too many values given, expected only 'days', "
-            "'passes', and 'unknown'")
     try:
-        return Repeat(*(f(s) for f, s in zip((float, int, float), parts)))
+        funcs: Sequence[Callable[[str], Any]] = (
+            float, int, float, lambda x: x)
+        return Repeat(*(f(s) for f, s in zip(funcs, parts)))
     except TypeError:
-        if parts:
-            raise TypeError("missing length of repeat cycle in 'passes'")
-        raise TypeError("missing length of repeat cycle in 'days'")
+        if not parts:
+            raise ValueError("missing length of repeat cycle in 'days'")
+        if len(parts) == 1:
+            raise ValueError("missing length of repeat cycle in 'passes'")
+        raise ValueError(
+            "too many values given, expected only 'days', "
+            "'passes', and 'longitude_drift'")
 
 
-def time(string: str) -> datetime:
+def time(string: str, _: Mapping[str, str]) -> datetime:
+    """Parse a string into a :class:`datetime` object.
+
+    Parameters
+    ----------
+    string
+        String to parse into a :class:`datetime` object. It should be in one of
+        the following forms:
+
+            * yyyy-mm-ddTHH:MM:SS
+            * yyyy-mm-ddTHH:MM
+            * yyyy-mm-ddTHH
+            * yyyy-mm-ddT
+            * yyyy-mm-dd
+    _
+        Mapping of tag attributes.  Not used by this function.
+
+    Returns
+    -------
+        A new :class:`datetime` object created from the parsed string.
+
+    Raises
+    ------
+    ValueError
+        If the date/time :paramref:`string` cannot be parsed.
+
+    """
+    return _time(string)
+
+
+def unit(string: str, _: Mapping[str, str]) -> Unit:
+    """Parse a string into a :class:`Unit` object.
+
+    .. _cf_units: https://github.com/SciTools/cf-units
+
+    .. _`issue 30`: https://github.com/SciTools/cf-units/issues/30
+
+    Parameters
+    ----------
+    string
+        String to parse into a :class:`Unit` object.  See the cf_units_ package
+        for supported units.  If given 'dB' or 'decibel' a no_unit object will
+        be returned and if given 'yymmddhhmmss' an unknown unit will be
+        returned.
+    _
+        Mapping of tag attributes.  Not used by this function.
+
+    Returns
+    -------
+    Unit
+        A new :class:`Unit` object created from the parsed string.  In the case
+        of 'dB' and 'decibel' this will be a no_unit and in the case of
+        'yymmddhhmmss' it will be 'unknown'.  See `issue 30`_.
+
+    Raises
+    ------
+    ValueError
+        If the given :paramref:`string` does not represent a valid unit.
+
+    """
+    try:
+        return Unit(string)
+    except ValueError:
+        string = string.strip()
+        # TODO: Remove this hack when
+        #  https://github.com/SciTools/cf-units/issues/30 is fixed.
+        if string in ('dB', 'decibel'):
+            return Unit('no unit')
+        if string == 'yymmddhhmmss':
+            return Unit('unknown')
+        raise ValueError(f"failed to parse unit '{string}'")
+
+
+def _time(string: str) -> datetime:
     try:
         return datetime.strptime(string, '%Y-%m-%dT%H:%M:%S')
     except ValueError:
@@ -164,16 +478,8 @@ def time(string: str) -> datetime:
                             "'%Y-%m-%dT%H:%M:%S'".format(string))
 
 
-def unit(string: str) -> Unit:
-    try:
-        return Unit(string)
-    except ValueError:
-        # TODO: Need better handling for dB and yymmddhhmmss units.
-        return string.strip()
-
-
 def _rads_type(string: str) -> type:
-    switch = {
+    switch: Dict[str, type] = {
         'int1': np.int8,
         'int2': np.int16,
         'int4': np.int32,
@@ -183,4 +489,4 @@ def _rads_type(string: str) -> type:
     try:
         return switch[string.lower()]
     except KeyError:
-        raise TypeError(f"invalid type string '{string}'")
+        raise ValueError(f"invalid RADS type string '{string}'")
---
+++ tests/config/test_text_parsers.py
@@ -0,0 +1,183 @@
+from datetime import datetime
+
+import numpy as np
+import pytest
+from cf_units import Unit
+
+from rads.config.text_parsers import (lift, list_of, range_of, one_of,
+                                      compress, cycles, nop, ref_pass, repeat,
+                                      time, unit)
+from rads.config.tree import Range, Compress, Cycles, ReferencePass, Repeat
+
+
+def test_lift():
+    assert lift(str)('3', {}) == '3'
+    assert lift(int)('3', {}) == 3
+    assert lift(float)('3.14', {}) == 3.14
+    with pytest.raises(ValueError):
+        lift(float)('not_a_float', {})
+    assert lift(int)._lifted == 'int'
+
+
+def test_list_of(mocker):
+    assert list_of(lift(int))('1 2 3 4 5', {}) == [1, 2, 3, 4, 5]
+    assert list_of(lift(int))('1   2 3      4 5', {}) == [1, 2, 3, 4, 5]
+    m = mocker.Mock()
+    m.return_value = 3
+    list_of(m)('abc 123', {'stuff': 3.14})
+    assert m.call_count == 2
+    m.assert_any_call('abc', {'stuff': 3.14})
+    m.assert_any_call('123', {'stuff': 3.14})
+
+
+def test_range_of(mocker):
+    assert range_of(lift(int))('2 3', {}) == Range(2, 3)
+    assert range_of(lift(float))('2.5 3.14', {}) == Range(2.5, 3.14)
+    assert range_of(lift(int))('2   3', {}) == Range(2, 3)
+    m = mocker.Mock()
+    range_of(m)('2.5 3.14', {'stuff': 3.14})
+    assert m.call_count == 2
+    m.assert_any_call('2.5', {'stuff': 3.14})
+    m.assert_any_call('3.14', {'stuff': 3.14})
+    with pytest.raises(ValueError):
+        range_of(lift(int))('2.5 3.14', {})
+    with pytest.raises(ValueError):
+        range_of(lift(int))(' ', {})
+    with pytest.raises(ValueError):
+        range_of(lift(int))('1', {})
+    with pytest.raises(ValueError):
+        range_of(lift(int))('1 2 3', {})
+
+
+def test_one_of(mocker):
+    int_float_str = one_of((lift(int), lift(float), lift(str)))
+    assert int_float_str('3', {}) == 3
+    assert type(int_float_str('3', {})) == int
+    assert int_float_str('3.14', {}) == 3.14
+    assert type(int_float_str('3.14', {})) == float
+    assert int_float_str('3abc', {}) == '3abc'
+    assert type(int_float_str('3abc', {})) == str
+    m1 = mocker.Mock()
+    m2 = mocker.Mock()
+    one_of((m1, m2))('abc', {'stuff': 123})
+    m1.assert_called_once_with('abc', {'stuff': 123})
+    assert m2.call_count == 0
+    with pytest.raises(ValueError):
+        one_of((lift(int), lift(float)))('abc', {})
+    with pytest.raises(ValueError):
+        one_of((time,))('abc', {})
+
+
+def test_compress():
+    assert compress('int1', {}) == Compress(np.int8)
+    assert compress('int2', {}) == Compress(np.int16)
+    assert compress('int4', {}) == Compress(np.int32)
+    assert compress('real', {}) == Compress(np.float32)
+    assert compress('dble', {}) == Compress(np.float64)
+    assert compress('int4 2e-3', {}) == Compress(np.int32, 2e-3)
+    assert compress('int4 2d-3', {}) == Compress(np.int32, 2e-3)
+    assert compress('int4 2e-3 1300e3', {}) == Compress(np.int32, 2e-3, 1300e3)
+    assert compress('int4 2d-3 1300d3', {}) == Compress(np.int32, 2e-3, 1300e3)
+    assert compress('int4 2-3 1300+3', {}) == Compress(np.int32, 2e-3, 1300e3)
+    assert (compress('int4   2d-3    1300d3', {}) ==
+            Compress(np.int32, 2e-3, 1300e3))
+    with pytest.raises(ValueError):
+        compress('', {})
+    with pytest.raises(ValueError):
+        compress('int', {})
+    with pytest.raises(ValueError):
+        compress('int2 2a-3', {})
+    with pytest.raises(ValueError):
+        compress('int2 2e-3 1300a3', {})
+    with pytest.raises(ValueError):
+        compress('int4 2e-3 1300e3 5', {})
+
+
+def test_cycles():
+    assert cycles('100 200', {}) == Cycles(100, 200)
+    assert cycles('  100    200       ', {}) == Cycles(100, 200)
+    with pytest.raises(ValueError):
+        cycles('100 2.5', {})
+    with pytest.raises(ValueError):
+        cycles('1.5 200', {})
+    with pytest.raises(ValueError):
+        cycles('', {})
+    with pytest.raises(ValueError):
+        cycles('100', {})
+    with pytest.raises(ValueError):
+        cycles('100 200 300', {})
+
+
+def test_nop():
+    assert nop('  abc   ', {}) == '  abc   '
+
+
+def test_ref_pass():
+    assert (ref_pass('2016-03-01T21:00:11 -41.5 116 105', {}) ==
+            ReferencePass(datetime(2016, 3, 1, 21, 0, 11), -41.5, 116, 105))
+    assert (ref_pass('2016-03-01T21:00:11 -41.5 116 105 3', {}) ==
+            ReferencePass(datetime(2016, 3, 1, 21, 0, 11), -41.5, 116, 105, 3))
+    # invalid values
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T99:00:11 -41.5 116 105 3', {})
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11 2016-03-01T21:00:11 116 105 3', {})
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11 -41.5 116.5 105 3', {})
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11 -41.5 116 105.5 3', {})
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11 -41.5 116 105 3.5', {})
+    # missing values
+    with pytest.raises(ValueError):
+        ref_pass('', {})
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11', {})
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11 -41.5', {})
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11 -41.5 116', {})
+    # too many values
+    with pytest.raises(ValueError):
+        ref_pass('2016-03-01T21:00:11 -41.5 116 105 3 5', {})
+
+
+def test_repeat():
+    assert repeat('9.91 254', {}) == Repeat(9.91, 254)
+    assert repeat('9.91 254 0.2', {}) == Repeat(9.91, 254, 0.2)
+    # invalid values
+    with pytest.raises(ValueError):
+        repeat('abc 254', {})
+    with pytest.raises(ValueError):
+        repeat('9.91 254.5', {})
+    with pytest.raises(ValueError):
+        repeat('9.91 254 abc', {})
+    # missing values
+    with pytest.raises(ValueError):
+        repeat('', {})
+    with pytest.raises(ValueError):
+        repeat('9.91', {})
+    # too many values
+    with pytest.raises(ValueError):
+        repeat('9.91 254 0.2 1', {})
+
+
+def test_time():
+    assert time('2012-05-07T14:57:08', {}) == datetime(2012, 5, 7, 14, 57, 8)
+    assert time('2012-05-07T14:57', {}) == datetime(2012, 5, 7, 14, 57)
+    assert time('2012-05-07T14', {}) == datetime(2012, 5, 7, 14)
+    assert time('2012-05-07T', {}) == datetime(2012, 5, 7)
+    assert time('2012-05-07', {}) == datetime(2012, 5, 7)
+    with pytest.raises(ValueError):
+        time('2012-05', {})
+    with pytest.raises(ValueError):
+        time('2012-05-07 14:57:08', {})
+
+
+def test_unit():
+    assert unit('km', {}) == Unit('km')
+    assert unit('dB', {}) == Unit('no unit')
+    assert unit('decibel', {}) == Unit('no unit')
+    assert unit('yymmddhhmmss', {}) == Unit('unknown')
+    with pytest.raises(ValueError):
+        unit('abc', {})
