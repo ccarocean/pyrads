a4fedf7eaaffb16ef43f0695b52c312e25e7fab2
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-07-16 09:27:20 -0600

    Add forgotten type change.

--- rads/_typing.py
+++ rads/_typing.py
@@ -6,7 +6,7 @@ from typing import Union, IO, Any, TYPE_CHECKING
 
 import numpy as np  # type: ignore
 
-__all__ = ['PathLike', 'PathOrFile', 'Real', 'Number', 'NumberOrArray']
+__all__ = ['PathLike', 'PathOrFile', 'Number', 'IntOrArray', 'NumberOrArray']
 
 if TYPE_CHECKING:
     # pylint: disable=unsubscriptable-object
@@ -16,10 +16,9 @@ else:
 
 PathOrFile = Union[PathLike, IO[Any], int]
 
-Real = Union[int, float, numbers.Real]
-
 # for the purpose of PyRADS bool as a number since it will act
 # as 0 or 1 when used as a number
 Number = Union[int, float, bool]
 
+IntOrArray = Union[int, np.generic, np.ndarray]
 NumberOrArray = Union[Number, np.generic, np.ndarray]
