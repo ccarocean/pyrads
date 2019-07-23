18e75830eee05f0502ce4de28f146db118a712c0
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-07-23 09:29:41 -0600

    Add some missing rads.utility tests.
    * xor
    * contains_sublist
    * merge_sublist
    * delete_sublist

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,5 +1,46 @@
 import pytest  # type: ignore
-from rads._utility import fortran_float
+from rads._utility import (xor, contains_sublist, merge_sublist,
+                           delete_sublist, fortran_float)
+
+
+def test_xor():
+    assert not xor(True, True)
+    assert xor(True, False)
+    assert xor(False, True)
+    assert not xor(False, False)
+
+
+def test_contains_sublist():
+    assert contains_sublist([1, 2, 3, 4], [1, 2])
+    assert contains_sublist([1, 2, 3, 4], [2, 3])
+    assert contains_sublist([1, 2, 3, 4], [3, 4])
+    assert contains_sublist([1, 2, 3, 4], [1, 2, 3, 4])
+    assert not contains_sublist([1, 2, 3, 4], [2, 1])
+    assert not contains_sublist([1, 2, 3, 4], [3, 2])
+    assert not contains_sublist([1, 2, 3, 4], [4, 3])
+    # while the empty list is technically a sublist of any list for this
+    # function [] is never a sublist
+    assert not contains_sublist([1, 2, 3, 4], [])
+
+
+def test_merge_sublist():
+    assert merge_sublist([1, 2, 3, 4], []) == [1, 2, 3, 4]
+    assert merge_sublist([1, 2, 3, 4], [1, 2]) == [1, 2, 3, 4]
+    assert merge_sublist([1, 2, 3, 4], [2, 3]) == [1, 2, 3, 4]
+    assert merge_sublist([1, 2, 3, 4], [3, 4]) == [1, 2, 3, 4]
+    assert merge_sublist([1, 2, 3, 4], [0, 1]) == [1, 2, 3, 4, 0, 1]
+    assert merge_sublist([1, 2, 3, 4], [4, 5]) == [1, 2, 3, 4, 4, 5]
+    assert merge_sublist([1, 2, 3, 4], [1, 1]) == [1, 2, 3, 4, 1, 1]
+
+
+def test_delete_sublist():
+    assert delete_sublist([1, 2, 3, 4], []) == [1, 2, 3, 4]
+    assert delete_sublist([1, 2, 3, 4], [1, 2]) == [3, 4]
+    assert delete_sublist([1, 2, 3, 4], [2, 3]) == [1, 4]
+    assert delete_sublist([1, 2, 3, 4], [3, 4]) == [1, 2]
+    assert delete_sublist([1, 2, 3, 4], [0, 1]) == [1, 2, 3, 4]
+    assert delete_sublist([1, 2, 3, 4], [4, 5]) == [1, 2, 3, 4]
+    assert delete_sublist([1, 2, 3, 4], [1, 1]) == [1, 2, 3, 4]
 
 
 def test_fortran_float():
