11b9167fa92b9d7d201ca88939fb7409392d89e3
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-07-26 08:30:29 -0600

    Export configuration loading at base module.

--- rads/__init__.py
+++ rads/__init__.py
@@ -3,6 +3,9 @@
 # NOTE: The major version should match the current major version of RADS.
 __version__ = '0.1.0rc'
 
+from .config.loader import ConfigError, config_files, get_dataroot, load_config
 from .constants import EPOCH
 
-__all__ = ['EPOCH']
+__all__ = ['ConfigError',
+           'config_files', 'get_dataroot', 'load_config',
+           'EPOCH']
