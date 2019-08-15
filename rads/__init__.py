075bc23b2e25137e436d35855e2d4f3501023d14
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-08-15 00:25:00 -0600

    Change get_dataroot to return None when none exists.

--- rads/__init__.py
+++ rads/__init__.py
@@ -1,14 +1,17 @@
 """Python library for the Radar Altimeter Database System (RADS)."""
 
 from .__version__ import __version__
-from .config.loader import ConfigError, config_files, get_dataroot, load_config
+from .config.loader import config_files, get_dataroot, load_config
+from .exceptions import RADSError, ConfigError, InvalidDataroot
 from .constants import EPOCH
 
 __all__ = [
     "__version__",
+    "EPOCH",
     "ConfigError",
+    "InvalidDataroot",
+    "RADSError",
     "config_files",
     "get_dataroot",
     "load_config",
-    "EPOCH",
 ]
