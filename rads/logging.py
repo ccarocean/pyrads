b1d82a0bdb9f79cdd4b98cd710c92fa191935565
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-10-16 18:35:54 -0600

    Add documentation to rads.logging.

--- rads/logging.py
+++ rads/logging.py
@@ -1,3 +1,5 @@
+"""Log instance and logging configuration."""
+
 import logging
 from logging import CRITICAL, DEBUG, ERROR, INFO, WARNING
 from typing import Union, Optional
@@ -14,9 +16,30 @@ __all__ = [
 ]
 
 log = logging.getLogger(__name__.split(".")[0])
+"""The PyRADS logger, use :func:`configure_logging` to set level and destination."""
 
 
-def configure_logging(level: Union[str, int], file: Optional[PathLike] = None):
+def configure_logging(level: Union[str, int], file: Optional[PathLike] = None) -> None:
+    """Configure logging for PyRADS.
+
+    This will automatically be called upon import with the following arguments
+    but can be called again to change logging.
+
+    .. code-block:: python
+
+        configure_logging(WARNING)
+
+    :param level:
+        The logging level, can be any of:
+            * :data:`DEBUG`
+            * :data:`INFO`
+            * :data:`WARNING` (PyRADS default)
+            * :data:`ERROR`
+            * :data:`CRITICAL`
+    :param file:
+        Optionally specify a filename to write the log to.  By default all log
+        messages will be written to *stderr*.
+    """
     for handler in log.handlers:
         log.removeHandler(handler)
 
